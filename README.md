7ff364e842213659e138c735fc1176a1559cd27d
Pavlo Ivanchyshyn <p.ivanchyshyn@gmail.com>
2018-03-24 09:49:00 +0200

    Added a simple documentation.

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# salary_reports
+===================
+Project description
+===================
+
+  - This project reads the data from .csv files and stores it into SQLite database
+  - Runs a Flask webserver to make it possible to review stored data via browser
+  - Added some basic filters on UI side to filter data by salary
+
+=============
+Prerequisites
+=============
+
+To start this project under Ubuntu (or EC2 instance with Ubuntu) you'll need:
+
+  - virtualenv (to isolate Python's installed libraries)
+
+===========
+Preparation
+===========
+
+  - Create your virtual environment (it's recommended to create it
+  in the project's directory):
+
+      `virtualenv venv`
+
+  - Activate your virtual environment:
+
+      `. venv/bin/activate`
+
+  - Install required Python's dependencies:
+
+      `pip install -r requirements.txt`
+
+  Well, your environment is ready!
+
+==================================================
+Move the data from .csv files into SQLite database
+==================================================
+
+  To move all the data from your .csv files stored in `data` folder please run:
+
+    `python create_databases.py`
+
+  If everything is OK, `salaries.sqlite` file will appear in your folder.
+
+============================
+Startup your web application
+============================
+
+  To start your web application please run:
+
+    `python web_app.py &`
+
+  It starts a webserver on `5000` port.
+
+  To make sure everything works fine, open your browser and check it:
+
+    http://<your_ip_address>:5000
+
+  If you're running this under AWS EC2 instance it requires to open inbound traffic for `5000` port.
--- web_app.py
+++ web_app.py
@@ -17,6 +17,7 @@ def home():
 
 @app.route('/payback')
 def display_payack():
+    # Get all data from `degrees_that_pay_back` table.
     df = pd.read_sql_query("SELECT * FROM degrees_that_pay_back", conn)
     return render_template("payback.html", data=df.to_html(
         classes=["table-bordered", "table-striped", "table-hover"]
@@ -25,6 +26,7 @@ def display_payack():
 
 @app.route('/colleges')
 def display_colleges():
+    # Get all data from `salaries_by_college_type` table.
     df = pd.read_sql_query("SELECT * FROM salaries_by_college_type", conn)
     return render_template("colleges.html", data=df.to_html(
         classes=["table-bordered", "table-striped", "table-hover"]
@@ -33,6 +35,7 @@ def display_colleges():
 
 @app.route('/regions')
 def display_regions():
+    # Get all data from `salaries_by_region` table.
     df = pd.read_sql_query("SELECT * FROM salaries_by_region", conn)
     return render_template("regions.html", data=df.to_html(
         classes=["table-bordered", "table-striped", "table-hover"]
@@ -43,21 +46,28 @@ def display_regions():
 def display_aggregation():
     salary = request.form["salary"]
     filter_field = request.form["filter"]
+
+    # Fetch filtered data from `degrees_that_pay_back` table.
     df_payback = pd.read_sql_query(
         """
         SELECT undergraduate_major, {} FROM degrees_that_pay_back
         WHERE {} > {}
         """.format(filter_field, filter_field, salary), conn)
+
+    # Fetch filtered data from `salaries_by_college_type` table.
     df_colleges = pd.read_sql_query(
         """
         SELECT school_name, school_type, {} FROM salaries_by_college_type
         WHERE {} > {}
         """.format(filter_field, filter_field, salary), conn)
+
+    # Fetch filtered data from `salaries_by_region` table.
     df_regions = pd.read_sql_query(
         """
         SELECT school_name, region, {} FROM salaries_by_region
         WHERE {} > {}
         """.format(filter_field, filter_field, salary), conn)
+
     return render_template(
         "aggregate.html",
         payback=df_payback.to_html(
